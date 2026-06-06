394cc777158a4a2e89cfb66a12180caee2c7d965
thaimun67 <thaimun.main@gmail.com>
2026-06-06 18:15:37 +0200

    Remove raw source files from repository tracking

--- Loader.lua
+++
@@ -1,4 +0,0 @@
--- Quantix Loader (Cache Bypass Version)
--- Paste this script into your executor
-local url = "https://raw.githubusercontent.com/thaimun67/Sokfweoifjwef0iowejfiowejfiowe/main/QuantixMain.lua?t=" .. tostring(tick())
-loadstring(game:HttpGet(url))()
